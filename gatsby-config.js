fa253ef112e0fbf7357c7b0ebc4b40fc82f681b2
Jasmine Ou <jasmineou99@gmail.com>
2019-02-16 17:09:55 -0500

    setup configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Meaningful Nonsense`,
+    description: `Blog 2.0`,
+    author: `JasmineOuO`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,17 +18,21 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Meaningful Nonsense`,
+        short_name: `Meaningful Nonsense`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#f7d1d3`,
+        theme_color: `#f7d1d3`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        include_favicon: true, // Include favicon
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+   'gatsby-plugin-offline',
   ],
+  pathPrefix: "/MeaningfulNonsense",
 }
