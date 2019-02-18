653654152d7fed2b75e27bb9664a0db19b9783b6
Jasmine Ou <jasmineou99@gmail.com>
2019-02-18 01:29:56 -0500

    Add redirect from netlify

--- src/components/SocialButton/SocialButton.js
+++ src/components/SocialButton/SocialButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './SocialButton.module.css';
 
 const SocialButton = ({ link, icon, title }) => (
-  <a className={classes.Link} href={link} target="_blank" title={title}>{icon}</a>
+  <a className={classes.Link} href={link} target="_blank" title={title} rel="noreferrer">{icon}</a>
 );
 
 export default SocialButton;
