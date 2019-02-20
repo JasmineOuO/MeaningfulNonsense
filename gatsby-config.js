50877521122bb4609ebbe2951a4ff40fa32c6bea
Jasmine Ou <jasmineou99@gmail.com>
2019-02-20 02:33:46 -0500

    Update CSP

--- gatsby-config.js
+++ gatsby-config.js
@@ -96,15 +96,14 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-csp', // https://github.com/bejamas/gatsby-plugin-csp
       options: {
-        disableOnDev: true,
+        disableOnDev: false,
         mergeScriptHashes: true, // you can disable scripts sha256 hashes
         mergeStyleHashes: true, // you can disable styles sha256 hashes
         mergeDefaultDirectives: true,
         directives: {
-          'script-src': "'self' www.google-analytics.com",
-          'style-src': "'self' 'unsafe-inline'",
-          'img-src': "'self' data: www.google-analytics.com",
-          // you can add your directives or override defaults
+          'script-src': "'self' https://identity.netlify.com/",
+          'style-src': "'self' blob: http://localhost:8000/ https://meaningfulnonsense.ca/ https://fonts.googleapis.com",
+          'font-src': "'self' https://fonts.gstatic.com",
         },
       },
     },
