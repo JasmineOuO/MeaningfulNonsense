b1724165cbe34c75da14e1dfe833b598dc71d3a3
Jasmine Ou <jasmineou99@gmail.com>
2019-02-20 01:07:17 -0500

    Add CSP plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 module.exports = {
   siteMetadata: {
     title: 'Meaningful Nonsense',
@@ -84,6 +85,27 @@ module.exports = {
         display: 'standalone',
         icon: 'src/images/icon.png',
         include_favicon: true,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        implementation: require('sass'),
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-csp', // https://github.com/bejamas/gatsby-plugin-csp
+      options: {
+        disableOnDev: true,
+        mergeScriptHashes: true, // you can disable scripts sha256 hashes
+        mergeStyleHashes: true, // you can disable styles sha256 hashes
+        mergeDefaultDirectives: true,
+        directives: {
+          'script-src': "'self' www.google-analytics.com",
+          'style-src': "'self' 'unsafe-inline'",
+          'img-src': "'self' data: www.google-analytics.com",
+          // you can add your directives or override defaults
+        },
       },
     },
     'gatsby-plugin-offline',
