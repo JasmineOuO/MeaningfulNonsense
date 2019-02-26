f71cd70801a6e1d719f98039179fa235671d8a8c
Jasmine Ou <jasmineou99@gmail.com>
2019-02-26 02:35:22 -0500

    Fix error

--- src/components/Animation/Particle.js
+++ src/components/Animation/Particle.js
@@ -9,81 +9,14 @@ export default function Particle(userOptions) {
     maxSize: 14,
     minSize: 9,
     newOn: 300,
-    swayAnimations: ['sway-0', 'sway-1', 'sway-2', 'sway-3', 'sway-4', 'sway-5', 'sway-6', 'sway-7', 'sway-8'],
-    excludeMobile: true
+    swayAnimations: ['sway-0', 'sway-1', 'sway-2', 'sway-3', 'sway-4', 'sway-5', 'sway-6', 'sway-7', 'sway-8']
   }
 
-  const isIE= navigator.userAgent.match(/msie/i);
-  const isIE6= navigator.userAgent.match(/msie 6/i);
-  const isMobile= navigator.userAgent.match(/mobile|opera m(ob|in)/i);
-  const isBackCompatIE= (isIE && document.compatMode === 'BackCompat');
   const windowHeight= document.documentElement.clientHeight || document.body.clientHeight || document.body.scrollHeight;
-  const windowWidth= (document.documentElement.clientWidth || document.body.clientWidth || document.body.scrollWidth) - (!isIE ? 8 : 0);
-    
-  const docFrag = document.createDocumentFragment();
-
-  const features = () => {
-    var getAnimationFrame;
-
-    const timeoutShim = callback => {
-      window.setTimeout(callback, 1000/20);
-    }
-
-    var _animationFrame = (window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        timeoutShim);
-
-    // apply to window, avoid "illegal invocation" errors in Chrome
-    getAnimationFrame = _animationFrame ? () => {
-      return _animationFrame.apply(window, arguments);
-    } : null;
-
-    var testDiv;
-
-    testDiv = document.createElement('div');
-
-    function has(prop) {
-
-      // test for feature support
-      var result = testDiv.style[prop];
-      return (result !== undefined ? prop : null);
-
-    }
-
-    // note local scope.
-    var localFeatures = {
-
-      transform: {
-        ie:  has('-ms-transform'),
-        moz: has('MozTransform'),
-        opera: has('OTransform'),
-        webkit: has('webkitTransform'),
-        w3: has('transform'),
-        prop: null // the normalized property value
-      },
-
-      getAnimationFrame: getAnimationFrame
-
-    };
-
-    localFeatures.transform.prop = (
-      localFeatures.transform.w3 ||
-      localFeatures.transform.moz ||
-      localFeatures.transform.webkit ||
-      localFeatures.transform.ie ||
-      localFeatures.transform.opera
-    );
-
-    testDiv = null;
-
-    return localFeatures;
-  }
+  const windowWidth= document.documentElement.clientWidth || document.body.clientWidth || document.body.scrollWidth;
 
   if (!window.requestAnimationFrame) {
-    window.requestAnimationFrame = (callback, element) => {
+    window.requestAnimationFrame = callback => {
       var currTime = new Date().getTime();
       var timeToCall = Math.max(0, 16 - (currTime - lastTime));
       var id = window.setTimeout(() => {
@@ -125,9 +58,7 @@ export default function Particle(userOptions) {
   }
 
   this.doStart = function() {
-    if (!particle.excludeMobile || !isMobile) {
-      createParticles();
-    }
+    createParticles();
   }
 
   return this;
