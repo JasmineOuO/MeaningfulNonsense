7ea29501b848c69d6c3998fc8a892e64a74fc781
Jasmine Ou <jasmineou99@gmail.com>
2019-03-04 21:40:51 -0500

    Edit notifcation config

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,40 +1,42 @@
 /* eslint-disable */
-exports.onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    'This application has been updated. '
-    + 'Reload to display the latest version?',
-  );
-
-  if (answer === true) {
-    window.location.reload();
-  }
-};
-
 exports.onServiceWorkerUpdateFound = () => {
+  const goToBlog = () => {
+    window.location.href = 'https://meaningfulnonsense.ca';
+  };
+
+  const closeNotification = () => {
+    navigator.serviceWorker.ready.then((registration) => {
+      registration.closeNotification();
+    });
+  };
+
   const showNotification = () => {
     Notification.requestPermission((result) => {
       if (result === 'granted') {
         navigator.serviceWorker.ready.then((registration) => {
-          registration.showNotification('Update', {
+          registration.showNotification('Meaningful Nonsense Update', {
+            badge: 'icons/mstile-70x70.png',
             body: 'New blog post!',
-            // icon: 'link-to-your-icon',
-            vibrate: [200, 100, 200, 100, 200, 100, 400],
+            icon: 'icons/favicon.ico',
+            vibrate: [200, 100, 200, 100, 400],
             tag: 'push-notification',
-            // actions: [ // you can customize these actions as you like
-            //   {
-            //     action: doSomething(), // you should define this
-            //     title: 'update',
-            //   },
-            //   {
-            //     action: doSomethingElse(), // you should define this
-            //     title: 'ignore',
-            //   },
-            // ],
+            actions: [ // you can customize these actions as you like
+              {
+                action: goToBlog(), // you should define this
+                title: 'Check it out',
+              },
+              {
+                action: closeNotification(), // you should define this
+                title: 'Ok',
+              },
+            ],
           });
         });
       }
     });
   };
 
-  showNotification();
+  if (Notification.permission !== 'blocked') {
+    showNotification();
+  }
 };
