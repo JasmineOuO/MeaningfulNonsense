901f7087b7ef3108fa9af3913430762657cf198e
Jasmine Ou <jasmineou99@gmail.com>
2019-03-05 00:53:00 -0500

    Remove sitemap and robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -186,15 +186,6 @@ module.exports = {
         ],
       },
     },
-    'gatsby-plugin-sitemap',
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        host: siteUrl,
-        sitemap: `${siteUrl}/sitemap.xml`,
-        policy: [{ userAgent: '*', disallow: '' }],
-      },
-    },
     'gatsby-plugin-offline',
     'gatsby-plugin-eslint',
     {
