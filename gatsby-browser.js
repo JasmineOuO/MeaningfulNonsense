3d6eae7bc7a84173eb62f4ca122e64ff83968447
Jasmine Ou <jasmineou99@gmail.com>
2019-03-04 20:52:10 -0500

    Add push notifications

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,3 +9,32 @@ exports.onServiceWorkerUpdateReady = () => {
     window.location.reload();
   }
 };
+
+exports.onServiceWorkerUpdateFound = () => {
+  const showNotification = () => {
+    Notification.requestPermission((result) => {
+      if (result === 'granted') {
+        navigator.serviceWorker.ready.then((registration) => {
+          registration.showNotification('Update', {
+            body: 'New blog post!',
+            // icon: 'link-to-your-icon',
+            vibrate: [200, 100, 200, 100, 200, 100, 400],
+            tag: 'push-notification',
+            // actions: [ // you can customize these actions as you like
+            //   {
+            //     action: doSomething(), // you should define this
+            //     title: 'update',
+            //   },
+            //   {
+            //     action: doSomethingElse(), // you should define this
+            //     title: 'ignore',
+            //   },
+            // ],
+          });
+        });
+      }
+    });
+  };
+
+  showNotification();
+};
