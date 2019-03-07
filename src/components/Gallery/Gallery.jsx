e1f656b968740f5469f7df87e9188e0340c1478a
Jasmine Ou <jasmineou99@gmail.com>
2019-03-06 22:42:36 -0500

    Tweak gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,4 +1,4 @@
-/*eslint-disable*/
+/* eslint-disable */
 import React from 'react';
 import { graphql, StaticQuery } from 'gatsby';
 
@@ -14,48 +14,43 @@ const Gallery = ({ data, numCols }) => {
     for (let i = 0; i < numCols; i++) {
       columns[`column${i}`] = [];
     }
-    posts.map(({ node: post }, index) => {
+    posts.forEach(({ node: post }, index) => {
       columns[`column${index % numCols}`].push(<Polaroid post={post} type="post" />);
     });
     for (let i = 0; i < numCols; i++) {
       gallery.push(
-        <div className={`${classes.Col} ${classes.Flex3}`}>
-          {columns[`column${i}`]}
-        </div>
-      )
+        <div className={`${classes.Col} ${classes.Flex3}`}>{columns[`column${i}`]}</div>
+      );
     }
   }
-  return (
-    <div className={classes.Gallery}>
-      {gallery}
-    </div>
-  );
+  return <div className={classes.Gallery}>{gallery}</div>;
 };
 
 export default () => (
   <StaticQuery
     query={graphql`
-    query GalleryQuery {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] },
-        filter: { frontmatter: { templateKey: { eq: "post" } }}
-      ) {
-        edges {
-          node {
-            excerpt(pruneLength: 100)
-            id
-            fields {
-              slug
-            }
-            frontmatter {
-              title
-              templateKey
-              date(formatString: "DD/MM/YY")
-              thumbnail {
-                childImageSharp {
-                  fluid(maxWidth: 240, quality: 64) {
-                    aspectRatio
-                    ...GatsbyImageSharpFluid_withWebp
+      query GalleryQuery {
+        allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___date] }
+          filter: { frontmatter: { templateKey: { eq: "post" } } }
+        ) {
+          edges {
+            node {
+              excerpt(pruneLength: 100)
+              id
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+                templateKey
+                date(formatString: "DD/MM/YY")
+                thumbnail {
+                  childImageSharp {
+                    fluid(maxWidth: 240, quality: 64) {
+                      aspectRatio
+                      ...GatsbyImageSharpFluid_withWebp
+                    }
                   }
                 }
               }
@@ -63,10 +58,7 @@ export default () => (
           }
         }
       }
-    }
     `}
-    render={(data, count) => (
-      <Gallery data={data} count={count} />
-    )}
+    render={(data, count) => <Gallery data={data} count={count} />}
   />
 );
