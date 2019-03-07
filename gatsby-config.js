a148f9732895001b6a9a5dbc34a73142057cf880
Jasmine Ou <jasmineou99@gmail.com>
2019-03-06 22:26:24 -0500

    change siteUrl

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     title: 'Meaningful Nonsense',
     description: 'A personal blog',
     author: 'JasmineOuO',
+    siteUrl: 'https://meaningfulnonsense.ca',
     navLinks: [
       {
         name: 'home',
