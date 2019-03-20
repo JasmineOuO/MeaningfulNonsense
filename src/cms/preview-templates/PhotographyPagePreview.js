1d42c757caa5a6f53bedc6331d1058680825c1ca
Jasmine Ou <jasmineou99@gmail.com>
2019-03-19 23:47:36 -0400

    Use entry to construct photography object for CMS preview

--- src/cms/preview-templates/PhotographyPagePreview.js
+++ src/cms/preview-templates/PhotographyPagePreview.js
@@ -2,7 +2,16 @@ import React from 'react';
 import { PhotographyPageTemplate } from '../../templates/photography';
 
 const PhotographyPagePreview = ({ entry }) => {
-  return <PhotographyPageTemplate photos={entry.getIn(['data', 'photos'])} />;
+  return (
+    <PhotographyPageTemplate
+      photos={entry.getIn(['data', 'photos']).map(photo => ({
+        image: photo.getIn(['image']),
+        title: photo.getIn(['title']),
+        date: photo.getIn(['date']),
+        location: photo.getIn(['location'])
+      }))}
+    />
+  );
 };
 
 export default PhotographyPagePreview;
