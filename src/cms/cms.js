8e5bed2cfb2feec79c67b954e9d60c38f7eb84f6
Jasmine Ou <jasmineou99@gmail.com>
2019-03-20 13:30:09 -0400

    remove authorNote widget

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -13,41 +13,41 @@ CMS.registerPreviewTemplate('about', AboutPagePreview);
 CMS.registerPreviewTemplate('photography', PhotographyPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
 
-CMS.registerWidget('authorNote', AuthorNoteControl, AuthorNotePreview);
+// CMS.registerWidget('authorNote', AuthorNoteControl, AuthorNotePreview);
 
-CMS.registerEditorComponent({
-  id: 'separator',
-  label: '---',
-  // A bogus field so that the component doesn't look weird when rendered:
-  fields: [{ label: 'Separator', widget: 'select', options: ['---'], default: '---' }],
-  // Never match anything so that the separator will be recognized as a horizontal rule when the document is reloaded:
-  pattern: /.^/,
-  toBlock() {
-    return '---';
-  },
-  toPreview() {
-    return '<hr>';
-  }
-});
+// CMS.registerEditorComponent({
+//   id: 'separator',
+//   label: '---',
+//   // A bogus field so that the component doesn't look weird when rendered:
+//   fields: [{ label: 'Separator', widget: 'select', options: ['---'], default: '---' }],
+//   // Never match anything so that the separator will be recognized as a horizontal rule when the document is reloaded:
+//   pattern: /.^/,
+//   toBlock() {
+//     return '---';
+//   },
+//   toPreview() {
+//     return '<hr>';
+//   }
+// });
 
-CMS.registerEditorComponent({
-  id: 'quote',
-  label: 'Quote',
-  fields: [
-    { name: 'quote', label: 'quote', widget: 'text' },
-    { name: 'cite', label: 'cite', widget: 'string' }
-  ],
-  pattern: /^quote ([^<]*) cite ([^<]*)/,
-  fromBlock: function(match) {
-    return {
-      quote: match[1],
-      cite: match[2]
-    };
-  },
-  toBlock: function(obj) {
-    return 'quote ' + obj.quote + ' cite ' + obj.cite;
-  },
-  toPreview: function(obj) {
-    return '<blockquote>' + obj.quote + '</blockquote><cite>' + obj.cite + '</cite>';
-  }
-});
+// CMS.registerEditorComponent({
+//   id: 'quote',
+//   label: 'Quote',
+//   fields: [
+//     { name: 'quote', label: 'quote', widget: 'text' },
+//     { name: 'cite', label: 'cite', widget: 'string' }
+//   ],
+//   pattern: /^quote ([^<]*) cite ([^<]*)/,
+//   fromBlock: function(match) {
+//     return {
+//       quote: match[1],
+//       cite: match[2]
+//     };
+//   },
+//   toBlock: function(obj) {
+//     return 'quote ' + obj.quote + ' cite ' + obj.cite;
+//   },
+//   toPreview: function(obj) {
+//     return '<blockquote>' + obj.quote + '</blockquote><cite>' + obj.cite + '</cite>';
+//   }
+// });
