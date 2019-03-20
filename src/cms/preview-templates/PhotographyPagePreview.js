9cf49b512e6f2b801db3fa7e2f8548ee66ba03f8
Jasmine Ou <jasmineou99@gmail.com>
2019-03-19 23:14:20 -0400

    Alter passed in list of objects to CMS preview

--- src/cms/preview-templates/PhotographyPagePreview.js
+++ src/cms/preview-templates/PhotographyPagePreview.js
@@ -2,7 +2,16 @@ import React from 'react';
 import { PhotographyPageTemplate } from '../../templates/photography';
 
 const PhotographyPagePreview = ({ widgetsFor }) => {
-  return <PhotographyPageTemplate photos={widgetsFor('photos').map(photo => photo.data)} />;
+  return (
+    <PhotographyPageTemplate
+      photos={widgetsFor('photos').map(photo => ({
+        image: photo.getIn(['data', 'image']),
+        title: photo.getIn(['data', 'title']),
+        date: photo.getIn(['data', 'date']),
+        location: photo.getIn(['data', 'location'])
+      }))}
+    />
+  );
 };
 
 export default PhotographyPagePreview;
