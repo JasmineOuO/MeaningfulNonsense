ab24c52422eb912131c97806ab27cddf958840e5
Jasmine Ou <jasmineou99@gmail.com>
2019-03-19 23:19:48 -0400

    Remove widgetsFor from photography preview

--- src/cms/preview-templates/PhotographyPagePreview.js
+++ src/cms/preview-templates/PhotographyPagePreview.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import { PhotographyPageTemplate } from '../../templates/photography';
 
-const PhotographyPagePreview = ({ widgetsFor }) => {
-  return (
-    <PhotographyPageTemplate
-      photos={widgetsFor('photos').map(photo => ({
-        image: photo.getIn(['data', 'image']),
-        title: photo.getIn(['data', 'title']),
-        date: photo.getIn(['data', 'date']),
-        location: photo.getIn(['data', 'location'])
-      }))}
-    />
-  );
+const PhotographyPagePreview = ({ entry }) => {
+  return <PhotographyPageTemplate photos={entry.getIn(['data', 'photos'])} />;
 };
 
 export default PhotographyPagePreview;
